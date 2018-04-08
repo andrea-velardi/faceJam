da926159b1f06992ed8f95979694a47a2e8f9013
mdotjr <34221923+mdotjr@users.noreply.github.com>
2018-04-08 12:17:00 -0500

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,45 @@
+//Execute this code once the DOM has fully loaded
+$(document).ready(function() {
+
+// <!--pseudo coded prokect from readme below--> 
+
+//api 
+2QHsXeBIxzksQEMcpY518MaCFUpcbhjy
+ 
+// <!--##Project Description: This project generates a song to match your mood. -->
+
+// <!--The user will upload their own photo via the upload form, using file type valiation. -->
+var thumb = $('img#thumb');        
+
+new AjaxUpload('userImageUpload', {
+    //this specifies where we want to post the Ajax upload. It's important to keep the urls in HTML so it passes through. 
+  action: $('form#newHotnessForm').attr('action'),
+  //post name of file form you want reflect in server
+  name: 'userimage',
+  //we added a class to indicate that the image is uploading
+  //diplay:none; must be the img tage so that the image that is uploaded is visible to users
+  onSubmit: function(file, extension) {
+    $('div.preview').addClass('loading');
+  },
+  //onComplete pushes the image through to upload. we unbind it so that the event only runs through once
+  //
+  onComplete: function(file, response) {
+    thumb.load(function(){
+      $('div.preview').removeClass('loading');
+      thumb.unbind();
+    });
+    //this runs the function to respond in the html.
+    thumb.attr('src', response);
+  }
+});
+
+});
+
+
+
+
+
+
 
 // <!--pseudo coded prokect from readme below--> 
  
