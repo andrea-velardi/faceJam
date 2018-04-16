d768892895c10b072bb7c73b2acba36d28df7d09
Andrea Velardi <>
2018-04-15 17:56:53 -0700

    updated logo and captured value of url

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,76 +1,126 @@
-//Execute this code once the DOM has fully loaded
 $(document).ready(function() {
 
-// <!--pseudo coded prokect from readme below--> 
+//grab the information from the user inputted url
+$("#submit").click(function(userUpload) {
+  userImage = $("#userImage").val(); 
+  console.log(userImage); 
+});
 
-//api 
-<<<<<<< HEAD
-// 2QHsXeBIxzksQEMcpY518MaCFUpcbhjy
-=======
-//   var config = {
-//     apiKey: "AIzaSyAZ014zFO7UiMyE4mD1RpAgyeFZIK13X8M",
-//     authDomain: "facejam-7e7f0.firebaseapp.com",
-//     databaseURL: "https://facejam-7e7f0.firebaseio.com",
-//     projectId: "facejam-7e7f0",
-//     storageBucket: "facejam-7e7f0.appspot.com",
-//     messagingSenderId: "592333101979"
-//   };
-//   firebase.initializeApp(config);
 
->>>>>>> 178d089355e57762575eff60f35649d1dbdec29f
- 
-// <!--##Project Description: This project generates a song to match your mood. -->
+  jQuery.ajaxPrefilter(function(options) {
+    if (options.crossDomain && jQuery.support.cors) {
+        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+    }
 
-// <!--The user will upload their own photo via the upload form, using file type valiation. -->
-var thumb = $('img#thumb');        
+  });
+  
+    var api_key = "PVQ-3aZ02DnPvmJenhhE5uu2sL3LXfsq"
+    var api_secret = "2QHsXeBIxzksQEMcpY518MaCFUpcbhjy"
+    var url = "https://api-us.faceplusplus.com/facepp/v3/detect"; 
+    var image_url = "https://d2lm6fxwu08ot6.cloudfront.net/img-thumbs/960w/RO7HF83PPB.jpg";
+    var query_string = $.param({ 
+        api_key: api_key, 
+        api_secret: api_secret, 
+        image_url: image_url, 
+        return_attributes : "emotion" 
+    })
 
-new AjaxUpload('userImageUpload', {
-    //this specifies where we want to post the Ajax upload. It's important to keep the urls in HTML so it passes through. 
-  action: $('form#newHotnessForm').attr('action'),
-  //post name of file form you want reflect in server
-  name: 'userimage',
-  //we added a class to indicate that the image is uploading
-  //diplay:none; must be the img tage so that the image that is uploaded is visible to users
-  onSubmit: function(file, extension) {
-    $('div.preview').addClass('loading');
-  },
-  //onComplete pushes the image through to upload. we unbind it so that the event only runs through once
-  //
-  onComplete: function(file, response) {
-    thumb.load(function(){
-      $('div.preview').removeClass('loading');
-      thumb.unbind();
+    var url_and_query = url + "?" + query_string; 
+    var renderFaceEmotion; 
+    $.ajax(url_and_query, {
+        type: "POST",
+    })
+    .then(function(response){
+        console.log(response.faces[0].attributes.emotion); 
+        //need to make emotionLevels = response 
     });
-    //this runs the function to respond in the html.
-    thumb.attr('src', response);
-  }
+
+    //temporary array to troubleshoot the next button
+    var songArray = ["https://firebasestorage.googleapis.com/v0/b/facejam-200816.appspot.com/o/Reaching.mp3?alt=media&token=6a7c55ad-1e11-45e6-be33-de3da668e82d", "https://firebasestorage.googleapis.com/v0/b/facejam-200816.appspot.com/o/01%20The%20Secret%20Thoughts%20of%20Housewives.wav?alt=media&token=82f23962-be0b-4b12-8833-a8c7455feac4", "assets/javascript/LittleLandslides.mp3", "assets/javascript/WithYou.mp3" ]
+    var currentTrack = 0
+    
+    var captainPlanet = $(".captain-planet");
+
+    // Gets Link for Theme Song
+    var audioElement = document.createElement("audio");
+    audioElement.setAttribute("src", songArray[currentTrack]);
+
+    // Theme Button
+    $("#play").on("click", function() {
+    audioElement.play();
+    })
+
+    $("#stop").on("click", function() {
+    console.log(audioElement.currentTime)
+    audioElement.pause();
+    audioElement.currentTime = 0; ///check documentation to see if STOP is an actual thing
+    })
+
+    $("#next").on("click", function() {
+    currentTrack = currentTrack + 1;
+    console.log(songArray[currentTrack]);
+    audioElement.setAttribute("src", songArray[currentTrack]);
+    audioElement.play();
+    console.log(songArray);
+    ;
+    })
+
+    // <!--if statements -->
+    var happiness = 0.999
+    var surprised = 0.6
+    var neutral = 0.5
+    var sadness = 0.4
+    var disgust = 0.3
+    var anger = 0.2
+    var fear = 0.00001
+
+    var emotions = {happiness:100, surprised:60, neutral:50, sadness:40, disgust:30, anger:20, fear:0};
+    //we are defining these variable to store which song link we will attached the emotion to once the song emotion is defined.
+    var happiness = []
+    var surprised = []
+    var neutral = []
+    var sadness = []
+    var disgust = []
+    var anger = []
+    var fear = []
+
+    var emotionLevels = {happiness:1, surprised:1, neutral:1, sadness:2, disgust:2, anger:90, fear:2}
+    var emotionLevelsArray = [];
+    var emotionTypes = ['anger', 'disgust', 'fear', 'happiness', 'neutral', 'sadness', 'surprised'];
+    // myFunction(response.foo.bar) // what does david need? string 'happy'? string 'http://urltoyoutubeplaylist'
+    var response = function myFunction(emotionLevels) { // arg is from api
+        emotionLevelsArray = Object.values(emotionLevels); // Object.keys() ?
+        emotionLevelsArray.push(emotionLevels.happiness);
+        emotionLevelsArray.push(emotionLevels.surprised);
+        emotionLevelsArray.push(emotionLevels.neutral);
+        emotionLevelsArray.push(emotionLevels.sadness);
+        emotionLevelsArray.push(emotionLevels.disgust);
+        emotionLevelsArray.push(emotionLevels.anger);
+        emotionLevelsArray.push(emotionLevels.fear);
+        [1,1,1,2,2,90,2];
+    }
+    // marcus logic controls player logic
+    var currentTrack = highestIndex;
+    var highestIndex = 0;
+    var highestEmotionValue = 0; // 1
+    for (i = 0; i < emotionLevelsArray.length; i++) {
+        1 > 0
+        1 > 1
+        90 > 2
+        if (emotionLevelsArray[i] > highestEmotionValue) {
+            highestIndex = i;
+            highestEmotionValue = emotionLevelsArray[i];
+        }
+      }
+
+    console.log(highestIndex);
+    console.log(highestEmotionValue);
+    // console.log(emotionTypes[highestIndex]);
+
+    return emotionTypes[highestIndex];
+
+//display song data in table
+//need to grab the value of the song title and then .text to the table or html to replace? 
+  
+    
 });
-
-});
-
-
-
-
-
-
-
-
-// <!--We use the face recognition API, -->
-// <!--Face++, -->
-// <!--to capture your expression in the photo -->
-// <!--and quantify your mood based on an algorithm of emotions. -->
-// <!--The highest scoring emotion will be used as the indicator for song choice -->
-// <!--from the SoundCloud API. -->
-// <!--The song played will be displayed in a table, listing; song title, artist, and release date. -->
-// <!--There will also be a sound bar displayed at bottom of the screen to pause, skip, and play.-->
-
-// <!--##Tasks: Create an HTML File and Link Bootstrap, -->
-// <!--jQuery, -->
-// <!--and other saved repositories Create wirefram (text, images, table and music player using bootstrap) - -->
-// <!--also create a naming system for buttons and containers. -->
-// <!--Create UI design using CSS (floating images and table) -->
-// <!--Create our Javascript Initiating API calls using Ajax - -->
-// <!--also adding paramaters to the API results JQuery onclick functions -->
-// <!--if statements -->
-// <!--for loop of songs -->
-// <!--via the API-->    
