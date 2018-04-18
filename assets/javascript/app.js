b746f49229ad5336e3d61005aa4e30aedeb8811f
Andrea Velardi <>
2018-04-18 17:39:54 -0500

    inputted progress bar and modal

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,16 +7,28 @@ $(document).ready(function() {
     //grab the information from the user inputted url
     $("#submit").click(function(userUpload) {
         picValue = $("#userImage").val();
-        callFaceApi(picValue);
+        //.jpg .jpeg .gif .png or .tiff
+
+        if(picValue.endsWith('.jpg') || picValue.endsWith('.jpeg') || picValue.endsWith('.png') || picValue.endsWith('.tiff') || picValue.endsWith('.gif')) {
+          console.log('Valid!')
+          callFaceApi(picValue);
         $('#dummyHead').attr('src' ,picValue); //grabbing the source attribute and passing the picValue through to the #dummyHead
+        } else {
+          console.log('Invalid!')
+          $("#myModal").removeClass('hidden')// 
+        }
+        
         //possible add in "preview" of picture
         // if (picValue !== '.jpg' || '.gif' || '.png') {
           //then upload the modal
           
-       
     });
 
-
+    $(".close").click(function(event) {
+      console.log('Clicked!');
+      $("#myModal").addClass('hidden')
+    });
+//cors - crossdomain issues - you have to allow apis to speak to each other
     jQuery.ajaxPrefilter(function(options) {
         if (options.crossDomain && jQuery.support.cors) {
             options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
@@ -47,74 +59,16 @@ $(document).ready(function() {
         audioElement.currentTime = 0; ///check documentation to see if STOP is an actual thing
     })
 
-    $("#next").on("click", function() {
-    currentTrack = currentTrack + 1;
-        console.log(songArray[currentTrack]);
-        audioElement.setAttribute("src", songArray[currentTrack]);
-        audioElement.play();
-        console.log(songArray);
-    ;
-    })
+    // $("#next").on("click", function() {
+    //   currentTrack = currentTrack + 1;
+    //   console.log(songArray[currentTrack]);
+    //   audioElement.setAttribute("src", songArray[currentTrack]);
+    //   audioElement.play();
+    //   console.log(songArray);
+    // })
 
-
-
-    // DO NOT NEED
-    // // <!--if statements -->
-    // var happiness = 0.999
-    // var surprised = 0.6
-    // var neutral = 0.5
-    // var sadness = 0.4
-    // var disgust = 0.3
-    // var anger = 0.2
-    // var fear = 0.00001
-
-    // var emotions = {happiness:100, surprised:60, neutral:50, sadness:40, disgust:30, anger:20, fear:0};
-    // //we are defining these variable to store which song link we will attached the emotion to once the song emotion is defined.
-    // var happiness = []
-    // var surprised = []
-    // var neutral = []
-    // var sadness = []
-    // var disgust = []
-    // var anger = []
-    // var fear = []
-
-    var emotionLevels = {happiness:1, surprised:1, neutral:1, sadness:2, disgust:2, anger:90, fear:2}
-    var emotionLevelsArray = [];
-    var emotionTypes = ['anger', 'disgust', 'fear', 'happiness', 'neutral', 'sadness', 'surprised'];
-    // myFunction(response.foo.bar) // what does david need? string 'happy'? string 'http://urltoyoutubeplaylist'
-    var response = function myFunction(emotionLevels) { // arg is from api
-        emotionLevelsArray = Object.values(emotionLevels); // Object.keys() ?
-        emotionLevelsArray.push(emotionLevels.happiness);
-        emotionLevelsArray.push(emotionLevels.surprised);
-        emotionLevelsArray.push(emotionLevels.neutral);
-        emotionLevelsArray.push(emotionLevels.sadness);
-        emotionLevelsArray.push(emotionLevels.disgust);
-        emotionLevelsArray.push(emotionLevels.anger);
-        emotionLevelsArray.push(emotionLevels.fear);
-        [1,1,1,2,2,90,2];
-    }
-    // marcus logic controls player logic
-    var currentTrack = highestIndex;
-    var highestIndex = 0;
-    var highestEmotionValue = 0; // 1
-    for (i = 0; i < emotionLevelsArray.length; i++) {
-        // 1 > 0
-        // 1 > 1
-        // 90 > 2
-        if (emotionLevelsArray[i] > highestEmotionValue) {
-            highestIndex = i;
-            highestEmotionValue = emotionLevelsArray[i];
-        }
-    }
-
-    console.log(highestIndex);
-    console.log(highestEmotionValue);
-    // console.log(emotionTypes[highestIndex]);
-
-    return emotionTypes[highestIndex];
-
-//display song data in table
-//need to grab the value of the song title and then .text to the table or html to replace? 
+  // display song data in table
+  // need to grab the value of the song title and then .text to the table or html to replace? 
 });
 
 
@@ -152,7 +106,8 @@ callFaceApi = function(picUrl){
             $("#artist").text("David Bazan");
             $("#releaseDate").text("2015");
         }
-        
+        //progress bar
+        $('.progress-bar').css('width', emotionObj.emotion.happiness+'%')// need to ensure % to "fill the bar"
         console.log('Success', emotionObj);
     })
 }
